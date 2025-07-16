598dd85974b3f31a58ae98aa531b171593b8cd05
Awofala Gbolahan <edawofal@gmail.com>
2025-07-16 17:56:19 +0100

    Fixed the positioning

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Privacy = () => {
   return (
-    <div className='text-center text-sm pb-6 fixed bottom-2 px-18 lg:px-14'>
+    <div className='text-center text-sm pb-6 fixed bottom-2 px-16 lg:px-14'>
         <p>Our <span className='font-medium text-[#FF7A00]'>Privacy Policy</span> and <span className='font-medium text-[#FF7A00]'>Terms of Service</span></p>
         <p className='text-center'>Your credentials are secured with end-to-end encryption</p>
     </div>
